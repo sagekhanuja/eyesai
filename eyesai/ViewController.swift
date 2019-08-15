ea348910673238569d0cfa5f939bd48810499bdb
Sage Khanuja <sagekhanuja@Sages-MacBook-Pro.local>
2019-08-14 21:33:35 -0700

    finished app

--- eyesai/ViewController.swift
+++ eyesai/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        func captureOutput( output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection){
+            
+            guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+            
+            guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {return}
+            
+            let request = VNCoreMLRequest(model: model){
+                (finishedReq, err) in
+                
+                guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+                
+                guard let firstObservation = results.first else {return}
+                
+                print(firstObservation.identifier, firstObservation.confidence)
+            }
+            
+            try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+            
+        }
+            
+        }
+        
+        
+        
     }
 
-
-}
 
